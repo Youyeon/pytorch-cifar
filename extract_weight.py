2941b5cf8f3f8313dc172ba83479298c4db3b911
Youyeon <yourain416@gmail.com>
2022-03-09 22:20:55 +0900

    sorted file names by layer order

--- extract_weight.py
+++ extract_weight.py
@@ -6,7 +6,11 @@ import torch.backends.cudnn as cudnn
 from torchsummary import summary
 from models import *
 
+import os
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+path = "./saved/parms/resnet18/"
 
 net = ResNet18()
 net = net.to(device)
@@ -24,9 +28,7 @@ acc = checkpoint['acc']
 best_loss = checkpoint['loss']
 start_epoch = checkpoint['epoch']
 
-f = open("./temp/meta.txt", "w")
-print(net, file=f)
-f.close()
+os.makedirs(path, exist_ok=True)
 
 for key, val in _dict.items():
     if 'num_batches' in key:
@@ -34,7 +36,28 @@ for key, val in _dict.items():
     w = val.cpu().detach().numpy()
     w_reshaped = w.reshape(w.shape[0],-1)
     w_reshaped = w_reshaped.flatten()
-
-    with open("./temp/"+key+".dat", "wb") as fp:
+ 
+    with open(path+key+".dat", "wb") as fp:
         w_reshaped.tofile(fp, format='f4')
     fp.close()
+
+# Layer list (weight O)
+f_ = open(path+"names.txt", "w")
+
+name_list = []
+for name, _ in net.named_parameters():
+    name = name.replace(".weight", "")
+    name = name.replace(".bias", "")
+    if name not in name_list:
+        name_list.append(name)
+    else:
+        continue
+    f_.write(name)
+    f_.write("\n")
+
+f_.close()
+
+#model structure
+f = open(path+"meta.txt", "w")
+print(net, file=f)
+f.close()
